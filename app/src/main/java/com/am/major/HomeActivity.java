4fef70924be11f944ebb42a11afa963da2b0a317
Akshay Kumar <mail.akshay04@gmail.com>
2020-05-20 06:36:25 +0530

    scan button intent

--- app/src/main/java/com/am/major/HomeActivity.java
+++ app/src/main/java/com/am/major/HomeActivity.java
@@ -2,13 +2,24 @@ package com.am.major;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class HomeActivity extends AppCompatActivity {
-
+private LottieAnimationView lottieAnimationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        lottieAnimationView = findViewById(R.id.animation_view);
+        lottieAnimationView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this, DetectorActivity.class));
+            }
+        });
     }
 }
